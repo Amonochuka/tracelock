491062a2ff80da5060aa3efa946b49d3a815d200
Amonoff <ochukaamon@gmail.com>
2026-02-08 18:38:43 +0300

    feat: add user registration endpoint with password hashing

---
+++ internal/auth/auth.go
@@ -0,0 +1,49 @@
+package auth
+
+import (
+	"database/sql"
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID           int
+	Name         string
+	Email        string
+	PasswordHash string
+	Role         string
+}
+
+// register a new user
+func Register(db *sql.DB, name, email, password string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	//insert into the DB
+	_, err = db.Exec(
+		"INSERT INTO users(name, email, password_hash) VALUES($1,$2,$3)",
+		name, email, string(hash),
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Authenticate user(tobe used for verifying logins)
+func Authenticate(db *sql.DB, email, password string) (*User, error) {
+	user := &User{}
+	row := db.QueryRow("SELECT id, name, email, password_hash, role FROM users WHERE email=$1", email)
+	err := row.Scan(&user.ID, &user.Name, &user.Email, &user.PasswordHash, &user.Role)
+	if err != nil {
+		return nil, errors.New("User not found")
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password)); err != nil {
+		return nil, errors.New("invalid password")
+	}
+	return user, nil
+}
---
+++ internal/httpapi/router.go
@@ -0,0 +1,52 @@
+package httpapi
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"tracelock/internal/auth"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func New(db *sql.DB) http.Handler {
+	r := chi.NewRouter()
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+
+	})
+
+	//regsiter endpoint
+	r.Post("/register", func(w http.ResponseWriter, r *http.Request) {
+		var req struct {
+			Name     string `json:"name"`
+			Email    string `json:"email"`
+			Password string `json:"password"`
+		}
+
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "invalid request body", http.StatusBadRequest)
+			return
+		}
+
+		if req.Name == "" || req.Email == "" || req.Password == "" {
+			http.Error(w, "all fields are required", http.StatusBadRequest)
+			return
+		}
+
+		err := auth.Register(db, req.Name, req.Email, req.Password)
+		if err != nil {
+			log.Println("Register error:", err)
+			http.Error(w, "could not register user: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("user regsitered succesfully"))
+	})
+
+	return r
+}
