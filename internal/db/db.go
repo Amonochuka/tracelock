7f5df7b833c6b4d5d18263dd397369a99ecd960c
Amonoff <ochukaamon@gmail.com>
2026-02-08 15:07:27 +0300

    task : db setup

---
+++ internal/db/db.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+func Open() (*sql.DB, error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASSWORD")
+	name := os.Getenv("DB_NAME")
+
+	//data source name
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, pass, name,
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
