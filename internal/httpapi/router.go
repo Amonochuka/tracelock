7b0ceda8a6e05e1f75eb99f5adfa670de99778c6
Amonoff <ochukaamon@gmail.com>
2026-02-22 16:45:00 +0300

    changes

--- internal/httpapi/router.go
+++ internal/httpapi/router.go
@@ -2,8 +2,6 @@ package httpapi
 
 import (
 	"database/sql"
-	"encoding/json"
-	"log"
 	"net/http"
 	"strconv"
 	"tracelock/internal/auth"
@@ -21,36 +19,11 @@ func New(db *sql.DB) http.Handler {
 	})
 
 	//regsiter endpoint
-	r.Post("/register", func(w http.ResponseWriter, r *http.Request) {
-		var req struct {
-			Name     string `json:"name"`
-			Email    string `json:"email"`
-			Password string `json:"password"`
-		}
-
-		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-			http.Error(w, "invalid request body", http.StatusBadRequest)
-			return
-		}
-
-		if req.Name == "" || req.Email == "" || req.Password == "" {
-			http.Error(w, "all fields are required", http.StatusBadRequest)
-			return
-		}
-
-		err := auth.Register(db, req.Name, req.Email, req.Password)
-		if err != nil {
-			log.Println("Register error:", err)
-			http.Error(w, "could not register user: "+err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte("user regsitered succesfully"))
-	})
+	r.Post("/register", auth.RegisterHandler(db))
 
 	//login route
-	
+	r.Post("/login", auth.LoginHandler(db))
+
 	//test JWT middleware
 	r.Group(func(r chi.Router) {
 		r.Use((auth.JWTMiddleware))
