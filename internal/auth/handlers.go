de66561ab34bf7c5017917295e8696eb3a47055f
Amonoff <ochukaamon@gmail.com>
2026-03-04 23:57:16 +0300

    feat:add json request validation and standardized error responses for /regisiter

--- internal/auth/handlers.go
+++ internal/auth/handlers.go
@@ -4,9 +4,14 @@ import (
 	"database/sql"
 	"encoding/json"
 	"net/http"
-
-	
+	"tracelock/internal/httpa"
 )
+
+type requestRegister struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
 // same email andp password in DB ?
 func LoginHandler(db *sql.DB) http.HandlerFunc {
@@ -48,31 +53,25 @@ func LoginHandler(db *sql.DB) http.HandlerFunc {
 
 func RegisterHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var request struct {
-			Name     string `json:"name"`
-			Email    string `json:"email"`
-			Password string `json:"password"`
-		}
-
-		if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-			http.Error(w, "invalid request body", http.StatusBadRequest)
+		var req requestRegister
+		dec := json.NewDecoder(r.Body)
+		dec.DisallowUnknownFields()
+		if err := dec.Decode(&req); err != nil {
+			httpa.WriteError(w, http.StatusBadRequest, "invalid json")
 			return
 		}
 
-		if request.Name == "" || request.Email == "" || request.Password == "" {
-			http.Error(w, "all fields are required", http.StatusBadRequest)
+		if req.Name == "" || req.Email == "" || req.Password == "" {
+			httpa.WriteError(w, http.StatusBadRequest, "all fields are required")
 			return
 		}
 
-		if err := Register(db, request.Name, request.Email, request.Password); err != nil {
-			http.Error(w, "could not register user", http.StatusInternalServerError)
+		if err := Register(db, req.Name, req.Email, req.Password); err != nil {
+			httpa.WriteError(w, http.StatusInternalServerError, "could not register user: "+err.Error())
 			return
 		}
-
-		w.WriteHeader(http.StatusCreated)
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]string{
-			"message": "user created successfully",
+		httpa.WriteJSON(w, http.StatusCreated, map[string]string{
+			"message": "user registered successfully",
 		})
 	}
 }
@@ -82,7 +81,7 @@ func MeHandler(db *sql.DB) http.HandlerFunc {
 		UserClaims, ok := r.Context().Value(UserContextKey).(*UserClaims)
 		if !ok {
 			http.Error(w, "unauthorized", http.StatusUnauthorized)
-			return 
+			return
 		}
 		uuid := UserClaims.UserID
 
@@ -94,7 +93,7 @@ func MeHandler(db *sql.DB) http.HandlerFunc {
 		}
 
 		err := db.QueryRow(`SELECT id, name, email, 
-		role FROM users WHERE id =$1`, uuid,).Scan(&user.ID, &user.Name, &user.Email, &user.Role)
+		role FROM users WHERE id =$1`, uuid).Scan(&user.ID, &user.Name, &user.Email, &user.Role)
 		if err != nil {
 			http.Error(w, "user not found", http.StatusNotFound)
 		}
