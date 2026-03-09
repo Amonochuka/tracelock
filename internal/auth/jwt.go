ca120f5f627e30188e94da5a681747574655adc0
aochuka <ochukaamon@gmail.com>
2026-03-09 15:01:35 +0300

    fix:change jwt from a global var to a service

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,40 +1,32 @@
 package auth
 
 import (
-	"fmt"
-	"os"
-	
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var jwtSecret []byte
-
-// load it from the environment
-func InitJWT() {
-	secret := os.Getenv("JWT_SECRET")
-	if secret == "" {
-		panic("JWT_SECRET not set")
-	}
-
-	jwtSecret = []byte(secret)
-	fmt.Println("JWT initialized succesfully")
+type JWTService struct {
+	secret []byte
 }
 
-func GenerateToken(user *User) (string, error) {
+func NewJWTService(secret string) *JWTService {
+	return &JWTService{secret: []byte(secret)}
+}
+
+func (j *JWTService) GenerateToken(user *User) (string, error) {
 	claims := jwt.MapClaims{
-		"sub": user.ID,
+		"sub":  user.ID,
 		"role": user.Role,
-		"exp": time.Now().Add(time.Hour * 24).Unix(),
-		"iat": time.Now().Unix(),
+		"exp":  time.Now().Add(time.Hour * 24).Unix(),
+		"iat":  time.Now().Unix(),
 	}
 
 	//create a token with HS356 signing
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	//sign token using jwtSecret
-	tokenString, err := token.SignedString(jwtSecret)
+	tokenString, err := token.SignedString(j.secret)
 	if err != nil {
 		return "", ErrTokenInvalidMethod
 	}
