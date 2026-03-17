a05b2db67e1db18359fb67b776fe11500a75c03c
Amonoff <ochukaamon@gmail.com>
2026-03-17 20:07:04 +0300

    feat:Add ZoneRepo struct together with methods

--- internal/access/repo.go
+++ internal/access/repo.go
@@ -1,19 +1,26 @@
 package access
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+)
 
-type ZoneRepo struct{
+type ZoneRepo struct {
 	db *sql.DB
 }
 
-func NewZoneRepo(db *sql.DB)*ZoneRepo{
+func NewZoneRepo(db *sql.DB) *ZoneRepo {
 	return &ZoneRepo{db: db}
 }
 
-func(z *ZoneRepo)
--Createevent
-GetLastEventHash
-GetLastEventHash
-CountActiveUsers
-CreateSession
-DeleteSession
+func (z *ZoneRepo) GetMaximumCapacity(ZoneID int) (int, error) {
+	var capacity int
+	err := z.db.QueryRow("SELECT max_capacity FROM zones WHERE id = $1", ZoneID).Scan(&capacity)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, errors.New("zone not found")
+		}
+		return 0, err
+	}
+	return capacity, nil
+}
