a753e84e92d3a58847dc11510fa76a11af6efcca
Amonoff <ochukaamon@gmail.com>
2026-03-20 12:33:18 +0300

    refactor: reorganize project structure

--- internal/access/access_service.go
+++ internal/access/access_service.go
@@ -13,7 +13,7 @@ func NewZoneService(repo *ZoneRepo) *ZoneService {
 	return &ZoneService{repo: repo}
 }
 
-func (s *ZoneService) EnterZone(userID, zoneID int, timestamp time.Time) error {
+func (s *ZoneService) EnterZone(userID, zoneID int, action string, timestamp time.Time) error {
 	capacity, err := s.repo.GetMaximumCapacity(zoneID)
 	if err != nil {
 		return err
@@ -33,9 +33,7 @@ func (s *ZoneService) EnterZone(userID, zoneID int, timestamp time.Time) error {
 		return err
 	}
 
-	previous_hash, _ := s.repo.GetLastHash(zoneID)
-	hash := GenerateHash(userID , zoneID int, action string,
-	timestamp time.Time, previousHash string)
-
-	return s.repo.CreateEvent(userID, zoneID, "enter", "success", hash, previous_hash)
+	previousHash, _ := s.repo.GetLastHash(zoneID)
+	hash := GenerateHash(userID, zoneID, action, timestamp, previousHash)
+	return s.repo.CreateEvent(userID, zoneID, "enter", "success", hash, previousHash)
 }
