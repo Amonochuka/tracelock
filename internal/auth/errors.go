ae08bbc387fc1f6d39e754f7cdf64d9e9558c53c
Amonoff <ochukaamon@gmail.com>
2026-03-21 12:05:50 +0300

    Revert "refactor:files to use errors.go"
    This reverts commit ad442da97ba05ee0b202c7140a337d64d19b3993.
    
    revert back to previous state

--- internal/auth/errors.go
+++ internal/auth/errors.go
@@ -3,8 +3,7 @@ package auth
 import "errors"
 
 var (
-	ErrTokenInvalidMethod = errors.New("invalid jwt signing method")
-	ErrUserNotFound       = errors.New("user not found")
-	ErrInvalidPassword    = errors.New("invalid password")
-	ErrEmailExists        = errors.New("email already exists")
+	ErrUserNotFound    = errors.New("user not found")
+	ErrInvalidPassword = errors.New("invalid password")
+	ErrEmailExists     = errors.New("email already exists")
 )
