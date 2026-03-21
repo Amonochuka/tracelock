8a803c2e9c5a4aa8118f6a8a1963458eff11adec
Amonoff <ochukaamon@gmail.com>
2026-03-21 12:09:02 +0300

    refactor:files to use errors.go

--- internal/auth/errors.go
+++ internal/auth/errors.go
@@ -3,7 +3,8 @@ package auth
 import "errors"
 
 var (
-	ErrUserNotFound    = errors.New("user not found")
-	ErrInvalidPassword = errors.New("invalid password")
-	ErrEmailExists     = errors.New("email already exists")
+	ErrTokenInvalidMethod = errors.New("invalid jwt signing method")
+	ErrUserNotFound       = errors.New("user not found")
+	ErrInvalidPassword    = errors.New("invalid password")
+	ErrEmailExists        = errors.New("email already exists")
 )
