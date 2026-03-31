f8bd7228a72af9a6ba38cc3bb673f2292f6b3ffe
aochuka <ochukaamon@gmail.com>
2026-03-31 14:54:52 +0300

    feat: add routes for enter and exit

--- internal/httpdir/router.go
+++ internal/httpdir/router.go
@@ -3,46 +3,47 @@ package httpdir
 import (
 	"net/http"
 	"strconv"
+
+	"tracelock/internal/access"
 	"tracelock/internal/auth"
 	"tracelock/internal/httpdir/middleware"
 
 	"github.com/go-chi/chi/v5"
 )
 
-func New(s *auth.UserService, jwtservice *auth.JWTService) http.Handler {
+func New(s *auth.UserService, jwtService *auth.JWTService, zoneService *access.ZoneService) http.Handler {
 	r := chi.NewRouter()
 
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("ok"))
-
 	})
 
-	//regsiter endpoint
 	r.Post("/register", RegisterHandler(s))
-
-	//login route
-	r.Post("/login", LoginHandler(s, jwtservice))
-
-	//test JWT middleware
+	r.Post("/login", LoginHandler(s, jwtService))
 
 	r.Group(func(r chi.Router) {
-		r.Use(auth.JWTMiddleware(jwtservice))
+		r.Use(auth.JWTMiddleware(jwtService))
+
+		r.Get("/me", MeHandler(s))
 
 		r.Get("/protected", func(w http.ResponseWriter, r *http.Request) {
 			user := auth.GetUserClaims(r)
 			w.Write([]byte("Hello user ID: " + strconv.Itoa(user.UserID) + " your role is: " + user.Role + "\n"))
 		})
+
 		r.Get("/testjwt", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("JWT middleware works!" + "\n"))
+			w.Write([]byte("JWT middleware works!\n"))
 		})
 
-		r.Get("/me", MeHandler(s))
+		// zone routes
+		r.Post("/zones/enter", EnterZoneHandler(zoneService))
+		r.Post("/zones/exit", ExitZoneHandler(zoneService))
 
-		r.With(middleware.RequireRole("admin")).Get("/admin/ping", (http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("admin ok" + "\n"))
-		})))
-
+		// admin routes
+		r.With(middleware.RequireRole("admin")).Get("/admin/ping", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("admin ok\n"))
+		})
 	})
 
 	return r
