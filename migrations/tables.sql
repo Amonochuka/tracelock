bc4a958ed1a75ceafeb45a583acd4e18b3f459d0
aochuka <ochukaamon@gmail.com>
2026-03-31 15:23:40 +0300

    fix :syntax error in sql file

--- migrations/tables.sql
+++ migrations/tables.sql
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS access_events(
 CREATE TABLE IF NOT EXISTS active_sessions (
     user_id INT NOT NULL REFERENCES users(id),
     zone_id INT NOT NULL REFERENCES zones(id),
-    entered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    entered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(user_id, zone_id)
 );
 
