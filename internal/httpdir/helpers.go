4cdab9ab60f16e86fdbab45f38a1b4e25a8bf4d4
Amonoff <ochukaamon@gmail.com>
2026-04-19 01:18:36 +0300

    feat : add various handlers

--- internal/httpdir/helpers.go
+++ internal/httpdir/helpers.go
@@ -10,3 +10,19 @@ import (
 func parseIDParam(r *http.Request, param string) (int, error) {
 	return strconv.Atoi(chi.URLParam(r, param))
 }
+
+func parsePagination(r *http.Request) (limit, offset int) {
+	limit = 20
+	offset = 0
+	if l := r.URL.Query().Get("limit"); l != "" {
+		if v, err := strconv.Atoi(l); err == nil && v > 0 && v <= 100 {
+			limit = v
+		}
+	}
+	if o := r.URL.Query().Get("offset"); o != "" {
+		if v, err := strconv.Atoi(o); err == nil && v >= 0 {
+			offset = v
+		}
+	}
+	return
+}
