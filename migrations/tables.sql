fd94fcc4e1e1e7510563b88005b29db17eb58f29
Amonoff <ochukaamon@gmail.com>
2026-04-20 20:59:27 +0300

    fix: add comma in uzer_zone_access table

--- migrations/tables.sql
+++ migrations/tables.sql
@@ -43,9 +43,9 @@ ON CONFLICT DO NOTHING;
 
 
 CREATE TABLE IF NOT EXISTS user_zone_access (
-    user_id INT NOT NULL REFERENCES users(id),
-    zone_id INT NOT NULL REFERENCES zones(id),
+    user_id    INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    zone_id    INT NOT NULL REFERENCES zones(id) ON DELETE CASCADE,
     granted_by INT REFERENCES users(id),
-    granted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    granted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(user_id, zone_id)
 );
