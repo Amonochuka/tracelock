e9eb0ce8eedc206fda421ee399c66aae60edbb0d
aochuka <ochukaamon@gmail.com>
2026-04-20 15:45:53 +0300

    fix: wrap errors in migrations file

--- internal/db/migrations.go
+++ internal/db/migrations.go
@@ -2,19 +2,20 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"os"
 )
 
-func runMigrations(db *sql.DB) error{
+func runMigrations(db *sql.DB) error {
 	sqlBytes, err := os.ReadFile("migrations/tables.sql")
-	if err != nil{
-		return err
+	if err != nil {
+		return fmt.Errorf("reading migrations: %w", err)
 	}
 
 	sqlStatement := string(sqlBytes)
 	_, err = db.Exec(sqlStatement)
-	if err != nil{
-		return err
+	if err != nil {
+		return fmt.Errorf("running migrations: %w", err)
 	}
 	return nil
 }
