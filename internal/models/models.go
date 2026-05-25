b3d203d5345514e273aae217724a6e8c0fd189cc
aochuka <ochukaamon@gmail.com>
2026-05-25 13:47:48 +0300

    feat: add Device and BiometricCredential models and update AccessEvent with device and entry method fields

--- internal/models/models.go
+++ internal/models/models.go
@@ -28,10 +28,31 @@ type AccessEvent struct {
 	Timestamp    time.Time `json:"timestamp"`
 	Hash         string    `json:"hash"`
 	PreviousHash string    `json:"previous_hash"`
+	DeviceID     *int      `json:"device_id,omitempty"`
+	EntryMethod  string    `json:"entry_method,omitempty"`
 }
 
 type ZoneOccupancy struct {
 	Zone
 	ActiveCount int     `json:"active_count"`
 	ActiveUsers []*User `json:"active_users"`
+}
+
+type Device struct {
+	ID        int       `json:"id"`
+	ZoneID    int       `json:"zone_id"`
+	Name      string    `json:"name"`
+	Type      string    `json:"type"`
+	Serial    string    `json:"serial"`
+	Active    bool      `json:"active"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type BiometricCredential struct {
+	ID             int       `json:"id"`
+	UserID         int       `json:"user_id"`
+	EntryMethod    string    `json:"entry_method"`
+	CredentialHash string    `json:"credential_hash"`
+	EnrolledAt     time.Time `json:"enrolled_at"`
+	Revoked        bool      `json:"revoked"`
 }
